e36c747962a22a4ead79e447136a5a99cf531f85
itsrusty <rusty3031@gmail.com>
2024-08-05 22:49:40 -0700

    [feat]: resolviendole al gafete SEMI TRAINEE ALABURGUER

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route, useRouteError } from "react-router-dom";
 import Header from "./components/common/Header";
 import PageNotFound from "./components/errors/PageNotFound";
-import FormAuthRegister from "./components/forms/FormAuthLogin";
 import "./index.css";
+import Register from "./pages/auth/Register";
 
 function HandleError(){
   const error = useRouteError()
@@ -23,7 +23,7 @@ function App() {
             errorElement={<HandleError />}
             action={()=> { throw new Response('error', { status: 404 }) }}
           />
-          <Route path="/app/auth/login-user" element={<FormAuthRegister />} />
+          <Route path="/app/auth/login-user" element={<Register />} />
           <Route path="*" element={<PageNotFound />}/>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/auth/Register.tsx
@@ -0,0 +1,7 @@
+import FormAuthLogin from "../../components/forms/FormAuthLogin"
+
+function Register() {
+  return (<FormAuthLogin/>)
+}
+
+export default Register
